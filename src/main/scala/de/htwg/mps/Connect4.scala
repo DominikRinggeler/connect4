9d25f826a8b9d990bc9f8da201254cc53b127a7b
dominikringgeler <dominik.ringgeler@web.de>
2015-10-12 11:05:48 +0200

    First Version Test

---
+++ src/main/scala/de/htwg/mps/Connect4.scala
@@ -0,0 +1,5 @@
+package de.htwg.mps
+
+object Connect4 extends App {
+  println("Hello, connect4")
+}
--- src/main/scala/de/htwg/mps/Model/GameField.scala
+++ src/main/scala/de/htwg/mps/Model/GameField.scala
@@ -4,6 +4,7 @@ package de.htwg.mps.Model
  * Created by dominikringgeler on 12.10.15.
  */
 class GameField {
+  // Test
 
   def getFieldStatus(row: Int,column: Int): Unit ={ //GameToken
 
