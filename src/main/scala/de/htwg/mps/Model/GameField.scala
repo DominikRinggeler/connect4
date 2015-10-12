a7e4444f57ffc63d24975169e738de8c21cd8e5a
dominikringgeler <dominik.ringgeler@web.de>
2015-10-12 11:19:06 +0200

    Second Version

--- src/main/scala/de/htwg/mps/Model/GameField.scala
+++ src/main/scala/de/htwg/mps/Model/GameField.scala
@@ -4,7 +4,7 @@ package de.htwg.mps.Model
  * Created by dominikringgeler on 12.10.15.
  */
 class GameField {
-  // Test 2
+  // Test 23
 
   def getFieldStatus(row: Int,column: Int): Unit ={ //GameToken
 
