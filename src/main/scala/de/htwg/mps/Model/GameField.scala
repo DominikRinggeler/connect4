e476f93b284b7e400cef87fdf7c3cc2cea59f6ca
dominikringgeler <dominik.ringgeler@web.de>
2015-10-19 10:23:56 +0200

    GameField logic

--- src/main/scala/de/htwg/mps/Model/GameField.scala
+++ src/main/scala/de/htwg/mps/Model/GameField.scala
@@ -3,18 +3,42 @@ package de.htwg.mps.Model
 /**
  * Created by dominikringgeler on 12.10.15.
  */
-class GameField {
-  // Test 23 jonas 2
+class GameField(rows:Int,columns: Int) {
 
-  def getFieldStatus(row: Int,column: Int): Unit ={ //GameToken
+  val fields = Array.ofDim[GameToken](columns,rows)
 
+  for(rowIndex <- 0 until rows){
+    for(columnIndex <- 0 until columns){
+      fields(rowIndex)(columnIndex)=null;
+    }
   }
 
-  def setFieldStatus(row: Int,gameToken: GameToken): Unit ={
-
+  def getFieldStatus(row: Int,column: Int): Boolean = {
+    if (column < 0 || column >= columns || row < 0 || row >= rows || fields(row)(column) == null) {
+      return false
+    }
+    return true;
   }
 
-  def initField {}
+  def getFieldToken(row: Int,column: Int):GameToken = {
+    if (getFieldStatus(row,column) ==true) {
+        fields(row)(column);
+    }
+      null
+  }
 
-
+  def setFieldStatus(column: Int,gameToken: GameToken): Unit ={
+    if (column < 0 || column >= columns){
+      var rowIndex =0;
+      while (getFieldStatus(rowIndex,column)){
+        rowIndex=rowIndex+1;
+      }
+      fields(rowIndex)(column)=gameToken
+    /*
+      for(index <- 0 until rows; if(!getFieldStatus(index,column)))
+         getFieldStatus(index+1)
+    }
+    */
+    }
+  }
 }
