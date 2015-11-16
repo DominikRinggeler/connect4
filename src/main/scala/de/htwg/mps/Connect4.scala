a453102e9888e2db5444cd6889bbb9e89a3194ff
Jonas Kaltenbach <jonaskaba@web.de>
2015-11-16 15:14:29 +0100

    second version gui

--- src/main/scala/de/htwg/mps/Connect4.scala
+++ src/main/scala/de/htwg/mps/Connect4.scala
@@ -1,6 +1,7 @@
 package de.htwg.mps
 
 import java.awt.Color
+import javax.swing.Box
 
 import de.htwg.mps.Controller.GameController
 import de.htwg.mps.Model.{GameField, GameToken, HumanPlayer}
@@ -26,96 +27,45 @@ object Connect4 extends App {
 
 object Connect4 extends SimpleSwingApplication {
 
-  GameField.initializeField(6,6)
+  var rows = 8
+  GameField.initializeField(rows,rows)
 
-  val s = new Dimension(50, 500)
+  val dCol = new Dimension(50, rows*45)
+  val dCell = new Dimension(40, 40)
 
-  val col1 =  new BoxPanel(Orientation.Vertical) {
-    minimumSize = s
-    maximumSize = s
-    preferredSize = s
-    background = Color.black
-    listenTo(mouse.clicks)
-    reactions += {
-      case e: MouseClicked =>
-        println("Mouse clicked at " + e.point)
+
+  lazy val ui = new FlowPanel()
+
+  for(index <- 0 until rows){
+    val col =  new BoxPanel(Orientation.Vertical) {
+      minimumSize = dCol
+      maximumSize = dCol
+      preferredSize = dCol
+      background = Color.black
+      listenTo(mouse.clicks)
+      reactions += {
+        case e: MouseClicked =>
+          println("Mouse clicked at " + e.point)
+      }
+
+      for(indexRow <- 0 until rows) {
+        contents += new Panel {
+          background = Color.white
+          minimumSize = dCell
+          maximumSize = dCell
+          preferredSize = dCell
+          border = Swing.EmptyBorder(5, 5, 5, 5)
+        }
+        peer.add(Box.createVerticalStrut(5))
+
+      }
     }
-  }
-
-  val col2 =  new BoxPanel(Orientation.Vertical) {
-    minimumSize = s
-    maximumSize = s
-    preferredSize = s
-    background = Color.black
-    listenTo(mouse.clicks)
-    reactions += {
-      case e: MouseClicked =>
-        println("Mouse clicked at " + e.point)
-    }
-  }
-
-  val col3 =  new BoxPanel(Orientation.Vertical) {
-    minimumSize = s
-    maximumSize = s
-    preferredSize = s
-    background = Color.black
-    listenTo(mouse.clicks)
-    reactions += {
-      case e: MouseClicked =>
-        println("Mouse clicked at " + e.point)
-    }
-  }
-
-  val col4 =  new BoxPanel(Orientation.Vertical) {
-    minimumSize = s
-    maximumSize = s
-    preferredSize = s
-    background = Color.black
-    listenTo(mouse.clicks)
-    reactions += {
-      case e: MouseClicked =>
-        println("Mouse clicked at " + e.point)
-    }
-  }
-
-  val col5 =  new BoxPanel(Orientation.Vertical) {
-    minimumSize = s
-    maximumSize = s
-    preferredSize = s
-    background = Color.black
-    listenTo(mouse.clicks)
-    reactions += {
-      case e: MouseClicked =>
-        println("Mouse clicked at " + e.point)
-    }
-  }
-
-  val col6 =  new BoxPanel(Orientation.Vertical) {
-    minimumSize = s
-    maximumSize = s
-    preferredSize = s
-    background = Color.black
-    listenTo(mouse.clicks)
-    reactions += {
-      case e: MouseClicked =>
-        println("Mouse clicked at " + e.point)
-    }
-  }
-
-  col6.contents += new Button { text = "1"
-  }
-  col6.contents += new Button { text = "2"
-  }
-  col6.contents += new Button { text = "3"
-  }
-
-  lazy val ui = new FlowPanel(col1, col2, col3, col4, col5, col6) {
-    border = Swing.EmptyBorder(10, 10, 10, 10)
-
+    ui.contents += col
   }
 
 
-  def top = new MainFrame {
+
+    def top = new MainFrame {
     title = "Hello to Connect 4!"
     contents = ui
   }
